f84e3cbfb14b7fd6825f23dc4ec2289a22ec1b68
matheus barbosa <zipmatheus@hotmail.com>
2022-11-07 19:35:27 -0300

    Configuracao do projeto!

--- src/main/java/senai/springjava/SpringJavaApplication.java
+++ src/main/java/senai/springjava/SpringJavaApplication.java
@@ -2,12 +2,21 @@ package senai.springjava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringJavaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJavaApplication.class, args);
+	}
+	
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "world") String name) {
+		return String.format("Hello %s!", name);
 	}
 
 }
