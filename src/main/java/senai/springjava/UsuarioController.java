b7ab86d9fead20cecf1af685d512b1d466236416
matheus barbosa <zipmatheus@hotmail.com>
2022-11-09 20:20:52 -0300

    Refatoracao do projeto implantando o metodo put e delete

--- src/main/java/senai/springjava/UsuarioController.java
+++ src/main/java/senai/springjava/UsuarioController.java
@@ -1,12 +1,17 @@
 package senai.springjava;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 //http://localhost:8080/api/usuario/add?nome=Matheus@email=zipmatheus@hotmail.com
@@ -18,17 +23,32 @@ public class UsuarioController {
 	@Autowired
 	UsuarioRepository uRepository;
 	
-	@PostMapping("/add")
-	public @ResponseBody String addUsuario(@RequestParam String nome, @RequestParam String email) {
-		Usuario objU = new Usuario();
-		objU.setNome(nome);
-		objU.setEmail(email);
+	@PostMapping("/")
+	public @ResponseBody Integer addUsuario(@RequestBody Usuario objU) {
 		uRepository.save(objU);
-		return "salvo";
+		return objU.getId();
 	}
 	
-	@GetMapping("/all")
+	@GetMapping("/")
 	public @ResponseBody Iterable<Usuario> BuscarUsuarios() {
 		return uRepository.findAll();
+	}
+	
+	// http://localhost:8080/api/usuario/2
+	@GetMapping("/{id}")
+		public @ResponseBody Optional<Usuario> buscarUsuario(@PathVariable Integer id) {
+			return uRepository.findById(id);
+	}
+	
+	@PutMapping("/")
+	public @ResponseBody Usuario atualizar(@RequestBody Usuario objU) {
+		uRepository.save(objU);
+		return objU;
+	}
+	
+	@DeleteMapping("/{id}")
+	public @ResponseBody String apagar(@PathVariable Integer id) {
+		uRepository.deleteById(id);
+		return "Ok, apagado!";
 	}
 }
